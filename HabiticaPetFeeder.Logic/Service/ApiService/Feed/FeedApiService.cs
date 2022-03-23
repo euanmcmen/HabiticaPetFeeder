42ddcb205fe505a17ddf957838cf4695f75ea579
Euan McMenemin <euanmcmen@hotmail.com>
2022-03-23 19:46:42 +0000

    live implementation of feed pets

--- HabiticaPetFeeder.Logic/Service/ApiService/Feed/FeedApiService.cs
+++ HabiticaPetFeeder.Logic/Service/ApiService/Feed/FeedApiService.cs
@@ -1,6 +1,9 @@
-﻿using HabiticaPetFeeder.Logic.Model;
+﻿using HabiticaPetFeeder.Logic.Client;
+using HabiticaPetFeeder.Logic.Model;
 using HabiticaPetFeeder.Logic.Model.ApiOperations;
 using HabiticaPetFeeder.Logic.Service.ApiService.Feed.Abstraction;
+using HabiticaPetFeeder.Logic.Service.Interfaces;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -8,8 +11,23 @@ namespace HabiticaPetFeeder.Logic.Service.ApiService.Feed;
 
 public class FeedApiService : IFeedApiService
 {
+    private readonly ILogger<FeedApiService> logger;
+    private readonly HabiticaApiClient habiticaApiClient;
+    private readonly IRateLimitingService rateLimitingService;
+
+    public FeedApiService(ILoggerFactory loggerFactory, HabiticaApiClient habiticaApiClient, IRateLimitingService rateLimitingService)
+    {
+        this.logger = loggerFactory.CreateLogger<FeedApiService>();
+        this.habiticaApiClient = habiticaApiClient;
+        this.rateLimitingService = rateLimitingService;
+    }
+
     public async Task<RateLimitedApiResponse> FeedPetFoodAsync(AuthenticatedRateLimitedApiRequest<PetFoodFeed> apiPetFoodFeedRequest)
     {
-        throw new NotImplementedException("Not ready for live.");
+        await rateLimitingService.WaitForRateLimitDelayAsync(apiPetFoodFeedRequest.RateLimitInfo); 
+
+        var apiResponse = await habiticaApiClient.FeedPetFoodAsync(apiPetFoodFeedRequest.Body);
+
+        return apiResponse;
     }
 }
